f23b621d115afaa3242509940f240eaac488e728
praveenweb <praveend.web@gmail.com>
2020-08-04 19:25:46 +0530

    update netlify url from .com to .app

--- src/components/Auth/auth0-variables.js
+++ src/components/Auth/auth0-variables.js
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
   domain: 'graphql-tutorials.auth0.com',
   clientId: 'P38qnFo1lFAQJrzkun--wEzqljVNGcWW',
-  callbackUrl: 'https://learn-hasura-todo-app.netlify.com/callback',
-  returnTo: 'https://learn-hasura-todo-app.netlify.com'
+  callbackUrl: 'https://learn-hasura-todo-app.netlify.app/callback',
+  returnTo: 'https://learn-hasura-todo-app.netlify.app'
 };
